218e2821cd5531b09c15046979595ea5cd799230
Rohan Choudhary <anandc.rohan484@gmail.com>
2020-02-23 20:24:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/truthdare/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.truthdare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button button;
+    private Random random = new Random();
+    private int lastDirection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void spin(View view){
+
+        int newDirection = random.nextInt(36000)+360;
+
+        float pivotX = imageView.getWidth()/2;
+        float pivotY = imageView.getHeight()/2;
+
+        Animation rotate = new RotateAnimation(lastDirection,newDirection,pivotX,pivotY);
+        rotate.setDuration(2500);
+        rotate.setFillAfter(true);
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                button.setEnabled(false);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                button.setEnabled(true);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        imageView.startAnimation(rotate);
+
+
+    }
+}
